6181aca8de824124edcec794e32b0492b96ad59e
Robinson Amianda <robinsonamianda5@gmail.com>
2023-11-20 22:50:35 +0300

    looping lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count = count -1
+    print("Happy New Year!")   
+pass
 
 def square_integers(int_list):
     # code goes here!
+    return int_list ** 2
     pass
 
 def fizzbuzz():
     # code goes here!
+    
+    for num in range(100):
+        num = num + 1
+        if num % 3 == 0 and num % 5 == 0 :
+            print("FizzBuzz")
+        elif num % 3 ==0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
+
